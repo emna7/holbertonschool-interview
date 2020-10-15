3a87356dff62e4d7d682c4f6570b2cd89ed1bc2b
emna7 <bhmemna7@gmail.com>
2020-10-15 15:41:06 +0100

    fix

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,8 @@
+#ifndef _SANDPILES_h_
+#define _SANDPILES_h_
+
+#include <stdio.h>
+void _print_grid(int grid[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* _SANDPILES_h_ */
