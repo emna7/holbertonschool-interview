ca1e777022f10d12b84548195753cdcb523d1709
emna7 <bhmemna7@gmail.com>
2021-01-28 17:11:20 +0100

    fix

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,37 +1,40 @@
 #include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
-* sorted_array_to_avl - creates an AVL tree from an array
-* @array: pointer to the array
-* @size: size of the array
-* Return: on success pointer the new avl tree, NULL otherwise
-*/
-avl_t *sorted_array_to_avl(int *array, size_t size)
+ *create_tree - Function that creates an avl tree
+ *@array: array
+ *@parent: parent
+ *@start: start
+ *@end: end
+ *Return: avl tree
+ */
+avl_t *create_tree(int *array, avl_t *parent, int start, int end)
 {
-avl_t *head = NULL;
-int edge = 0;
-if (!array || !size)
+int middle;
+avl_t *tree;
+if (start > end)
 return (NULL);
-if (size % 2 == 0)
-edge = ((int)size / 2) - 1;
-else
-edge = (int)size / 2;
-head = binary_tree_node(array[edge]);
-return (head);
+middle = (start + end) / 2;
+tree = malloc(sizeof(avl_t));
+if (tree == NULL)
+return (NULL);
+tree->parent = parent;
+tree->n = array[middle];
+tree->left = create_tree(array, tree, start, middle - 1);
+tree->right = create_tree(array, tree, middle + 1, end);
+return (tree);
 }
 /**
-* binary_tree_node - creates a new node
-* @value: value to enter in the node
-* Return: new node of NULL if it fails
-*/
-avl_t *binary_tree_node(int value)
+ *sorted_array_to_avl - Function that builds an AVL tree from an array
+ *@array: array
+ *@size: size
+ *Return: avt tree
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-avl_t *new = NULL;
-new = malloc(sizeof(*new));
-if (!new)
+int siz = size - 1;
+if (size <= 0 || !array)
 return (NULL);
-new->n = value;
-new->left = NULL;
-new->parent = NULL;
-new->right = NULL;
-return (new);
+return (create_tree(array, NULL, 0, siz));
 }
