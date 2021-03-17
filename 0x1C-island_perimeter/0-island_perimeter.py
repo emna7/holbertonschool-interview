541b740c72e6e102e295bc4220cdd5a193ec8f38
emna7 <bhmemna7@gmail.com>
2021-03-17 17:23:09 +0100

    create

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+island_perimeter function
+"""
+
+
+def island_perimeter(grid):
+        """
+        returns the perimeter of island in grid
+        """
+        # Perimeter = number of edges
+        # edge detection along horizontal and vertical direction
+
+        def edges(matrix):
+            """
+            detect number of edges along horizontal direction
+            """
+            count = 0
+            for row in matrix:
+                row = [0] + row + [0] # deal with edge case
+                for i in range(1, len(row)):
+                    count += row[i] != row[i-1]
+                # print(count)
+            return count
+
+        tgrid = [[] for _ in range(len(grid[0]))]  # tranposed matrix
+        for row in grid:
+            for i, item in enumerate(row):
+                tgrid[i].append(item)
+
+        return edges(grid) + edges(tgrid)
